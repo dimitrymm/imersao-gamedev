4322f9050eb6becc1155533224ce9bff5e2578c2
Dimitry Machado Marinho <dimitrym.marinho@gmail.com>
2020-06-23 11:42:51 -0300

    update javascript

--- sketch.js
+++ sketch.js
@@ -1,11 +1,40 @@
 
+let imagemCenario;//variavel de escopo global, ou seja visivel para todo o programa
+let imagemPersonagem;
+let cenario;
+
+let somDoJogo;
+
+let personagem;
 
 
-function setup(){
-    createCanvas(windowWidth,windowHeight);
+
+function preload() {
+    imagemCenario = loadImage("imagens/cenario/floresta.png");
+    imagemPersonagem = loadImage("imagens/personagem/correndo.png");
+    somDoJogo = loadSound("sons/trilha_jogo.mp3");
 }
-function draw(){
-    background(200);
-    circle(mouseX,mouseY,200);
+
+//Esta funcao setup é iniciada antes do jogo iniciar
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    cenario = new Cenario(imagemCenario, 3);
+    personagem = new Personagem(imagemPersonagem);
+    frameRate(40);
+    somDoJogo.loop();
+
 }
+
+//Quando se deseja realizar alguma animacao ou funcao rotativa
+function draw() {
+    //image(imagemCenario,-50,0,width,height);
+    // image(imagemCenario,width-50,0,width,height);
+    cenario.exibe();
+    cenario.move();
+
+    personagem.exibe();
+
+
+}
+
 
